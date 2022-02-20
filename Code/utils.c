db68594f54d007e330f007e1079529dd3d3444f6
Benni <benjamin.jank.ges@gmx.de>
2022-02-20 23:24:12 +0200

    awd

--- Code/utils.c
+++ Code/utils.c
@@ -1,8 +1,28 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
-void convolve(double *inputMatrix, int d1_inputMatrix, int d2_inputMatrix, int h_inputMatrix, int w_inputMatrix, double *kernel, int d1_kernel, int d2_kernel, int h_kernel, int w_kernel, int stride) {
+double* convolve(
+        double* inputMatrix, int64_t d1_inputMatrix, int64_t d2_inputMatrix, int64_t h_inputMatrix, int64_t w_inputMatrix,
+        double* kernel, int64_t d1_kernel, int64_t d2_kernel, int64_t h_kernel, int64_t w_kernel, int64_t stride,
+        int64_t* d1_out, int64_t* d2_out, int64_t* d3_out, int64_t* d4_out, int64_t* d5_out
+    ) {
 
-    double result[d1_kernel][d2_kernel][d2_inputMatrix][h_inputMatrix-h_kernel+1][w_inputMatrix-w_kernel+1];
+    *d1_out = d1_kernel;
+    *d2_out = d2_kernel;
+    *d3_out = d2_inputMatrix;
+    *d4_out = h_inputMatrix - h_kernel + 1;
+    *d5_out = w_inputMatrix - w_kernel + 1;
+
+    const size_t size = *d1_out * *d2_out * *d3_out * *d4_out * *d5_out;
+    double* result = malloc(size * sizeof(double));
+
+    if(result == NULL)
+    {
+        fprintf(stderr, "Konnte keinen Speicher auf Heap für einen Array mit %d bytes reservieren.", size * sizeof(double));
+        return NULL;
+    }
+
     for (size_t f=0; f<d1_kernel; f++) {
       for (size_t h=0; h<d2_kernel; h++) {
         for (size_t g=0; g<d2_inputMatrix; g++) {
@@ -25,7 +45,7 @@ void convolve(double *inputMatrix, int d1_inputMatrix, int d2_inputMatrix, int h
                 sum_entirewindow += sum_onelineofwindow;
               }
               //printf("%lf\n", sum_entirewindow);
-              result[j][h][g][i][j] = sum_entirewindow;
+              *(result + f*(*d2_out)*(*d3_out)*(*d4_out)*(*d5_out) + h*(*d3_out)*(*d4_out)*(*d5_out) + g*(*d4_out)*(*d5_out) + i*(*d5_out) + j) = sum_entirewindow;
             }
           }
         }
@@ -36,7 +56,8 @@ void convolve(double *inputMatrix, int d1_inputMatrix, int d2_inputMatrix, int h
 
 }
 
+/*
 void test(double *inputMatrix, int d1, int d2, int h, int w, int i, int j, int k, int l) {
   double val = *(inputMatrix + d2*h*w*i + h*w*j + w*k + l);
   printf("%lf\n", val);
-}
+} */
